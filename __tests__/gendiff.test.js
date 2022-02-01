c9d8fe78097c4d3e06f7613f3436bb0379b6b8b8
erzhan <erzhan.sarsenov@gmail.com>
2022-02-01 19:31:15 +0600

    feat: step 4.2

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,15 +1,15 @@
-import genDiff from "../src/gendiff";
+import genDiff from '../src/gendiff.js';
 
 describe('geDiff', () => {
   it('#test1', () => {
-    const expected = "{\n" +
-    "  - follow: false,\n" +
-    "    host: hexlet.io,\n" +
-    "  - proxy: 123.234.53.22,\n" +
-    "  - timeout: 50,\n" +
-    "  + timeout: 20,\n" +
-    "  + verbose: true\n" +
-    "}";
+    const expected = '{\n'
+    + '  - follow: false,\n'
+    + '    host: hexlet.io,\n'
+    + '  - proxy: 123.234.53.22,\n'
+    + '  - timeout: 50,\n'
+    + '  + timeout: 20,\n'
+    + '  + verbose: true\n'
+    + '}';
     const actual = genDiff('__fixtures__/file1.json', '__fixtures__/file2.json');
     expect(actual).toBe(expected);
   });
