02821de7fda4bd56fda706d8da6f040499ddb15e
erzhan <erzhan.sarsenov@gmail.com>
2022-02-08 20:53:57 +0600

    feat: step 5.5

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
+console.log('gendiff start');
 import { Command } from 'commander';
 import genDiff from '../src/gendiff.js';
 
 const program = new Command();
 
-console.log('gendiff start');
 
 program
   .version('0.0.1')
