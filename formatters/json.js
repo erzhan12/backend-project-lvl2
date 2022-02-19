da4b1630e86967a92c0367dbbc0f895af691cd52
erzhan <erzhan.sarsenov@gmail.com>
2022-02-19 13:45:45 +0600

    fix: step 7.3

--- formatters/json.js
+++ formatters/json.js
@@ -1,6 +1,8 @@
 const json = (tree) => {
   const jsonString = JSON.stringify(tree, null, 2);
-  console.log(jsonString);
+  // console.log(jsonString);
+  // const jsonObject = JSON.parse(jsonString);
+  // console.log(jsonObject);
   return jsonString;
 };
 
